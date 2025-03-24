82c692e52dab71171b8d57e9fe52eb6d3df7d619
Omar Chabane <122582715+Felgu@users.noreply.github.com>
2025-03-24 11:57:59 -0400

    Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
-import Logo from '../assets/images/logo.jpg'
-import { Link } from 'react-router-dom'
-import ReorderIcon from '@mui/icons-material/Reorder';
-import '../styles/Navbar.css'
+import Link from 'next/link';
+import Image from 'next/image';
+import {FaHospital} from 'react-icons/fa';
 
 const Navbar = () => {
-  const [openLinks, setOpenLinks] = useState(false);
-  
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
-  };
   return (
-    <div className='navbar'>
-      <div className='logoleft' id={openLinks ? "open" : "close"}>
-        <img src={Logo} alt='logo'/>
-        <div className='hiddenLinks'>
-          <Link to="/">Accueil</Link>
-          <Link to="/menu">Menu</Link>
-          <Link to="/nous">Nous</Link>
-          <Link to="/contact">Contact</Link>          
+    <nav className="bg-[#154c79] p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+      
+        {/* Logo avec un lien */}
+        <Link href="/" className="flex items-center space-x-4">
+          {/* Utilisation du composant Image pour afficher le logo */}
+          <Image
+            src="/assets/notitia.png" 
+            alt="Logo"
+            width={80}
+            height={80}
+            className="text-white rounded-lg"
+          />
+          <span className="text-white text-4xl font-semibold">Notitia</span>
+          {/* <FaHospital size={30} className="text-white" /> */}
+        </Link>
+
+        {/* Liens de navigation */}
+        <div className="space-x-16">
+          <Link href="/" className="text-white hover:text-yellow-400">Accueil</Link>
+          <Link href="/about" className="text-white hover:text-yellow-400">À propos</Link>
+          <Link href="/contact" className="text-white hover:text-yellow-400">Contact</Link>
+          <Link href="/login" className="text-white bg-[#79154c] p-4 rounded-lg hover:text-[#ffffff] hover:bg-[#e28743]">Se connecter</Link>
+          <Link href="/signup" className="text-white bg-[#747915] p-4 rounded-lg hover:text-[#ffffff]">Créer un compte</Link>
         </div>
       </div>
-      <div className='ongletsdroite'>
-        <Link to="/">Accueil</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/nous">Nous</Link>
-        <Link to="/contact">Contact</Link>
-        <button onClick={toggleNavbar}>
-          <ReorderIcon />
-        </button>
-      </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
 export default Navbar;
